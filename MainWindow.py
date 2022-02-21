d2d6ec7cfc5f0f0d47fe3778a758cbb9413bc462
elimac00 <elimac250@gmail.com>
2022-02-21 10:26:39 +0100

    1.push

--- MainWindow.py
+++ MainWindow.py
@@ -1,5 +1,7 @@
-from PyQt6.QtWidgets import QMainWindow, QLabel, QPushButton, QHBoxLayout, QWidget
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
 from PyQt6.QtGui import QPixmap
+
 
 class MainWindow(QMainWindow):
 
@@ -16,18 +18,54 @@ class MainWindow(QMainWindow):
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
 
+        text = QLabel(self)
+        text.setText("Bild")
+        text.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        layout.addWidget(text)
+
         first_button = QPushButton(self)
+        first_button.setText("verkleinern!")
+        first_button.setToolTip("Hilfetext")
+        first_button.setToolTipDuration(10 * 200)
+        first_button.clicked.connect(self.smaller)
 
+        second_button = QPushButton(self)
+        second_button.setText("vergrößern!")
+        second_button.setToolTip("Hilfetext")
+        second_button.setToolTipDuration(10 * 200)
+        second_button.clicked.connect(self.bigger)
 
-        button_2 = QPushButton(self)
-        button_2.setText("hallo")
-        button_2.setToolTip("noooothing")
+        self.picture = QPixmap("pragser_wildsee_lago_di_braies_suedtirol_italien_GettyImages-1179457554.jpg")
+        self.pictureLabel = self.picture.scaled(600, 450)
 
+        self.label = QLabel()
+        self.label.setPixmap(self.pictureLabel)
 
-        first_button.setText("Mein erster QPushButton!")
-        first_button.setToolTip("Das ist der Hinweistext")
-        first_button.setToolTipDuration(10*200)
+        textLabel = QLabel
+        textLabel.setText("Bildvergrößerer")
+
+        layout.addWidget(textLabel, 0, 1)
+        layout.addWidget(first_button, 2, 0)
+        layout.addWidget(second_button, 2, 2)
+        layout.addWidget(self.label, 1, 0, 1, 0)
+
+        #third_button = QPushButton(self)
+        #third_button.setText("Naja!")
+        #third_button.setToolTip("Hilfetext")
+        #third_button.setToolTipDuration(10 * 200 )
 
         layout.addWidget(first_button)
+        layout.addWidget(second_button)
+        #layout.addWidget(third_button)
+
+
+    def smaller(self):
+        self.pictureLabel = self.piture.scaled(400, 300)
+        self.label.setPixmap(self.picture)
+
+    def bigger(self):
+        self.pictureLabel = self.picture.scaled(800,600)
+        self.label.setPixmap(self.picture)
 
 
