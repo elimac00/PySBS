1843376f99d4e5b14639801e9796d8dfa36ef9cf
elimac00 <elimac250@gmail.com>
2022-03-14 14:24:04 +0100

    14.03.2022 Datenabnk API

---
+++ test.py
@@ -0,0 +1,25 @@
+#startdesignerwithqt5-toolsdesigner
+import sys
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QGraphicsView, QPushButton
+from PyQt6 import uic
+
+class MyDialog(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyDialog, self).__init__()
+        uic.loadUi("Hallo2.ui", self)
+        self.__btn1 = self.findChild(QPushButton, "btn1")
+        self.__btn1.startsStop.clicked.connect(self.StartStopGol)
+
+
+
+    def btn_test(self):
+        print("clicked!!!")
+
+
+
+app=QtWidgets.QApplication(sys.argv)
+window=MyDialog()
+window.show()
+sys.exit(app.exec())
+
